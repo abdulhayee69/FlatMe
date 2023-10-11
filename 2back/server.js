01ecd717cd2901604b175e11189ddebf388233b9
baig <abdulhayeebag559@gmail.com>
2023-10-11 16:06:08 +0500

    sir dard

--- 2back/server.js
+++ 2back/server.js
@@ -1,28 +1,51 @@
-const { Pool } = require('pg');
 
-// Create a new PostgreSQL client pool
-const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'listingz_listings',
-  password: '1234',
-  port: 5432, // usually 5432
+const express = require("express");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
+const fileUpload = require("express-fileupload");
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(fileUpload());
+
+const db = mysql.createConnection({
+  host: "http://170.64.158.74",
+  user: "root",
+  password: "",
+  database: "allListings",
 });
 
-// Test the connection
-pool.query('SELECT NOW()', (err, res) => {
+db.connect((err) => {
   if (err) {
-    console.error('Error connecting to PostgreSQL:', err);
-  } else {
-    console.log('Connected to PostgreSQL database at:', res.rows[0].now);
+    console.error("Database connection failed: " + err.stack);
+    return;
   }
+  console.log("Connected to the database");
 });
 
-// Example query
-pool.query('SELECT * FROM listings', (err, res) => {
-  if (err) {
-    console.error('Error executing query:', err);
-  } else {
-    console.log('Query result:', res.rows);
+
+app.post("/upload", (req, res) => {
+  if (!req.files) {
+    return res.status(400).send("No files were uploaded.");
   }
+
+  const image = req.files.image; 
+
+  const listingData = req.body;
+
+  db.query("INSERT INTO listings SET ?", listingData, (err, results) => {
+    if (err) {
+      console.error("Error inserting data: " + err);
+      return res.status(500).send("Error inserting data.");
+    }
+
+    return res.status(200).send("Data inserted successfully.");
+  });
+});
+
+
+const port = 8080;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
